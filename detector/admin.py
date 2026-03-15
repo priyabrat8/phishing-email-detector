7330511375c8114b2e067e74457a0a58159ab2aa
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-15 09:55:53 +0530

    chnages to admin panel

--- detector/admin.py
+++ detector/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ScanResult
 # Register your models here.
+
+@admin.register(ScanResult)
+class ScanResultAdmin(admin.ModelAdmin):
+    list_display = ['email_text', 'result', 'risk_score', 'created_at',]
+    list_filter = ['risk_score', 'created_at', 'result']
+    search_fields = ['email_text']
