a2e0998de44d5969226c78addd82cb6daa1dc6c3
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-15 09:37:06 +0530

    Add models to save results

--- detector/models.py
+++ detector/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ScanResult(models.Model):
+    email_text = models.TextField()
+    email_sender = models.EmailField()
+    result = models.CharField(max_length=50)
+    risk_score = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
