5fcfb2219316362b5728fe569682af397217e99e
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-15 18:39:01 +0530

    update about app and its url in project

--- phishing_detector/settings.py
+++ phishing_detector/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'detector.app.DetectorConfig',
+    'detector.apps.DetectorConfig',
 ]
 
 MIDDLEWARE = [
