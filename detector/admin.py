79f9625fe5311ef04a2d32b850bb3d6fecee88be
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-16 17:14:07 +0530

    update models and admin panel to store phishing url in databse

--- detector/admin.py
+++ detector/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import ScanResult
+from .models import ScanResult, PhishingURL
 # Register your models here.
 
 @admin.register(ScanResult)
 class ScanResultAdmin(admin.ModelAdmin):
     list_display = ['email_text', 'result', 'risk_score', 'created_at',]
-    list_filter = ['risk_score', 'created_at', 'result']
+    list_filter = ['result']
     search_fields = ['email_text']
+
+@admin.register(PhishingURL)
+class PhishingURLAdmin(admin.ModelAdmin):
+    list_display = ['domain', 'source', ]
+    list_filter = ['source']
+    search_fields = ['domain']
--- detector/models.py
+++ detector/models.py
@@ -8,3 +8,19 @@ class ScanResult(models.Model):
     result = models.CharField(max_length=50)
     risk_score = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.result
+
+class PhishingURL(models.Model):
+    domain = models.CharField(max_length=255, db_index=True, unique=True)
+    source = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.domain
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['domain']),
+        ]
