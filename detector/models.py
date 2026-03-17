0a7b63b4ce4bf5dc8931ed69106c12587583ac06
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-17 20:28:22 +0530

    update id to make it random and safe

--- detector/models.py
+++ detector/models.py
@@ -1,8 +1,14 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 class ScanResult(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False
+    )
     email_text = models.TextField()
     email_sender = models.EmailField()
     result = models.CharField(max_length=50)
@@ -13,6 +19,11 @@ class ScanResult(models.Model):
         return self.result
 
 class PhishingURL(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False
+    )
     domain = models.CharField(max_length=255, db_index=True, unique=True)
     source = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
