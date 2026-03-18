e121a6ca9852429bcfac5ce299e499688d56c813
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-19 02:19:50 +0530

    add error handeling

--- detector/sender_validation.py
+++ detector/sender_validation.py
@@ -3,12 +3,14 @@ from .domain_checker import check_domain_age, check_domain_reputation
 def sender_validation(email_sender):
     score = 0
     reasons = []
-    
-    is_suspicious_domain, domain_age = check_domain_age(email_sender.split('@')[1])
+    try:
+        is_suspicious_domain, domain_age = check_domain_age(email_sender.split('@')[1])
 
-    if is_suspicious_domain:
-        score += 15
-        reasons.append(f"Sender's Domain very new ({domain_age} days old)")
+        if is_suspicious_domain:
+            score += 15
+            reasons.append(f"Sender's Domain very new ({domain_age} days old)")
+    except:
+        pass
     
     if check_domain_reputation(email_sender.split('@')[1]):
         score += 10
