53b2bd6037fd872f17456cf6461648a1e21168f1
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-19 21:09:47 +0530

    add other pages urls

--- detector/urls.py
+++ detector/urls.py
@@ -5,4 +5,6 @@ app_name = 'detector'
 urlpatterns = [
     path('', view=views.home, name='home'),
     path('scan/', view=views.scan_email, name='scan_email'),
+    path('contact/', view=views.contact, name='contact'),
+    path('terms/', view=views.terms, name='terms'),
 ]
