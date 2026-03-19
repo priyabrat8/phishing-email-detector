6478956dedfbcf4c247eb3fb5cbc64051e191377
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-19 21:08:45 +0530

    update admin panel view

--- detector/admin.py
+++ detector/admin.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 from .models import ScanResult, PhishingURL
 # Register your models here.
 
-@admin.register(ScanResult)
-class ScanResultAdmin(admin.ModelAdmin):
-    list_display = ['email_text', 'result', 'risk_score', 'created_at',]
-    list_filter = ['result']
-    search_fields = ['email_text']
+admin.site.register(ScanResult)
 
 @admin.register(PhishingURL)
 class PhishingURLAdmin(admin.ModelAdmin):
