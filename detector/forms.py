75e2a374e7b97f87d0e83a9cd2caf2eadd759cdc
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-19 21:10:32 +0530

    add tailwind css in forms

--- detector/forms.py
+++ detector/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 
+INPUT_CLASSES = 'w-full mt-1 rounded-lg border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-gray-900 bg-white p-2.5'
+
 class EmailPostForm(forms.Form):
-    email_sender = forms.EmailField(required=True, label="Sender's Email")
+    email_sender = forms.EmailField(required=True, label="Sender's Email", widget=forms.TextInput(attrs={
+            'placeholder': 'e.g., security@paypal-update.com',
+            'class': INPUT_CLASSES
+        }))
     email_text = forms.CharField(
     required=True,
     label="Email Body",
-    widget=forms.Textarea,)
+    widget=forms.Textarea(attrs={
+            'rows': 5, 
+            'placeholder': 'Paste the full body of the suspicious email here...',
+            'class': INPUT_CLASSES
+        }))
